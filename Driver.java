e9c1fb5b1387f136119ed2e569adab88e727d8c8
Farheen Shuja <farheen.shuja@mail.utoronto.ca>
2025-06-02 19:23:31 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,39 @@
+package Labs;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Driver {
-	public static void main(String [] args) {
-	Polynomial p = new Polynomial();
-	System.out.println(p.evaluate(3));
-	double [] c1 = {6,0,0,5};
-	Polynomial p1 = new Polynomial(c1);
-	double [] c2 = {0,-2,0,0,-9};
-	Polynomial p2 = new Polynomial(c2);
-	Polynomial s = p1.add(p2);
-	System.out.println("s(0.1) = " + s.evaluate(0.1));
-	if(s.hasRoot(1))
-		System.out.println("1 is a root of s");
-	else
-		System.out.println("1 is not a root of s");
+	public static void main(String [] args) throws FileNotFoundException {
+	
+		File content = new File("readFile.txt");
+		Polynomial p5 = new Polynomial(content);
+		p5.saveToFile("writeFile.txt");
+		
+		double [] a1 = {2,1};
+		int [] a2 = {1,2};
+		Polynomial p1 = new Polynomial(a1,a2);
+		
+		double [] b1 = {5,3,4};
+		int [] b2 = {2,1,0};
+		Polynomial p2 = new Polynomial(b1,b2);
+		
+		double [] c1 = {1};
+		int [] c2 = {1};
+		Polynomial p3 = new Polynomial(c1,c2);
+		
+		double [] d1 = {1};
+		int [] d2 = {1};
+		Polynomial p4 = new Polynomial(d1,d2);
+		
+		Polynomial s = p1.multiply(p2);
+		Polynomial t = p3.multiply(p4);
+		
+		System.out.println("s(0.2) = " + s.evaluate(0.2));
+		System.out.println("s(2) = " + t.evaluate(2));
+		
+		if(s.hasRoot(1))
+			System.out.println("1 is a root of s");
+		else
+			System.out.println("1 is not a root of s");
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,13 +1,73 @@
+package Labs;
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 public class Polynomial {
 	
 	double coeffs[];
+	int exp[];
 	public Polynomial() {
 		coeffs = new double[0];
+		exp = new int[0];
 	}
 	
-	public Polynomial(double given[]) {
+	public Polynomial(double given[], int given_exp[]) {
 		coeffs = new double[given.length];
 		coeffs = given.clone();
+		exp = new int[given_exp.length];
+		exp = given_exp.clone();
+	}
+	
+	public Polynomial (File content) throws FileNotFoundException {
+		Scanner scan = new Scanner(content);
+		String poly = scan.nextLine();
+		String[] terms = poly.split("(?=-)|(?=\\+)");
+		
+		int length = terms.length;
+		coeffs = new double[length];
+		exp = new int[length];
+		
+		for(int i = 0; i < length; i++) {
+			if (terms[i].contains("x")) {
+				String[] idv = terms[i].split("x");
+				coeffs[i] = Double.parseDouble(idv[0]);
+				exp[i] = Integer.parseInt(idv[1]);
+			}
+			else {
+				coeffs[i] = Double.parseDouble(terms[i]);
+				exp[i] = 0;
+			}
+		}
+	}
+	
+	public void saveToFile(String writeFile) {
+		String poly = "";
+		for(int i = 0; i < exp.length; i++) {
+			if (exp[i] != 0) {
+				if (coeffs[i]<0)
+				{
+					poly = poly + coeffs[i] + "x" + exp[i];
+				}
+				else
+				{
+					poly = poly + "+" + coeffs[i] + "x" + exp[i];
+				}
+			}
+			else {
+				poly = poly + coeffs[i];
+			}
+		}
+		try{
+			FileWriter wr = new FileWriter("writeFile");
+			wr.write(poly);
+			wr.close();
+		}
+		
+		catch (IOException e){
+			e.printStackTrace();
+		}
 	}
 	
 	public double[] getCoeffs()
@@ -15,41 +75,77 @@ public class Polynomial {
 		return coeffs;
 	}
 	
-	public Polynomial add(Polynomial a) {
+	public int[] getExp()
+	{
+		return exp;
+	}
 
-		
-		double[] coeffs2 = new double[a.getCoeffs().length];
-		coeffs2 = a.getCoeffs().clone();
-
-		int smallpoly = coeffs2.length;
-		int onebigger = 0;
-		if (coeffs.length < coeffs2.length) {
-			smallpoly = coeffs.length;
-		}
-		else {
-			onebigger = 1;
-		}
-		
-		if (onebigger == 0) {
-			for (int i = 0; i < smallpoly; i++) {
-				coeffs2[i] = coeffs[i] + coeffs2[i];
+	public int combined(int exp1[], int exp2[])
+	{
+		int total = exp1.length + exp2.length;
+		int same = 0;
+		for (int i = 0; i < exp1.length; i++) {
+			for (int j = 0; j < exp2.length; j++) {
+				if (exp1[i] == exp2[j]) {
+					same = same + 1;
+				}
 			}
-			return new Polynomial(coeffs2);	
+			
 		}
-		else {
-			for (int i = 0; i < smallpoly; i++) {
-				coeffs[i] = coeffs[i] + coeffs2[i];
-			}
-			return new Polynomial(coeffs);	
-		}
-
+		return total-same;
 	}
 	
+	public Polynomial add(Polynomial a)
+	{
+		double[] coeffs2 = new double[a.getCoeffs().length];
+		coeffs2 = a.getCoeffs().clone();
+		int[] exp2 = new int[a.getExp().length];
+		exp2 = a.getExp().clone();
+		
+		int sum = combined(exp, exp2);
+		double[] sumcoeffs = new double[sum];
+		int[] sumexp = new int[sum];
+		
+		boolean match = false;
+		for(int i = 0; i < exp.length; i++) {
+			match = false;
+			for (int j = 0; j < exp2.length; j++) {
+				if (exp[i] == exp2[j]) {
+					sumcoeffs[i] = coeffs[i] + coeffs2[j];
+					sumexp[i] = exp[i];
+					match = true;
+				}
+			}
+			if (match == false) {
+				sumcoeffs[i] = coeffs[i];
+				sumexp[i] = exp[i];
+			}
+		}
+		
+		match = false;
+		int end = exp.length;
+		for(int n = 0; n < exp2.length; n++) {
+			match = false;
+			for(int k = 0; k < exp.length; k++) {
+				if (exp2[n] == sumexp[k] && sumcoeffs[k] != 0) {
+					match = true;
+				}
+			}
+			if (match == false) {
+				sumcoeffs[end] = coeffs2[n];
+				sumexp[end] = exp2[n];
+				end = end + 1;
+			}
+
+		}
+		return new Polynomial(sumcoeffs,sumexp);
+	}
+
 	public double evaluate(double x)
 	{
 		double sum=0;
 		for (int i = 0; i < coeffs.length; i++) {
-			sum = sum + coeffs[i]*Math.pow(x, i);
+			sum = sum + coeffs[i]*Math.pow(x, exp[i]);
 		}
 		return sum;
 	}
@@ -60,6 +156,50 @@ public class Polynomial {
 			return true;
 		else
 			return false;
+	}
+	
+	public int check(int[] arr, int num) {
+		int index = -1;
+		for(int i = 0; i < arr.length; i++) {
+			if(num == arr[i]) {
+				index = i;
+			}
+		}
+		return index;
+	}
+	
+	public Polynomial multiply(Polynomial a) {
+		double[] coeffs2 = new double[a.getCoeffs().length];
+		coeffs2 = a.getCoeffs().clone();
+		int[] exp2 = new int[a.getExp().length];
+		exp2 = a.getExp().clone();
+		
+		int sum = exp.length + exp2.length;
+		double[] sumcoeffs = new double[sum];
+		int[] sumexp = new int[sum];
+		
+		int sum_p = 0;
+		for(int i = 0; i < exp.length; i++) {
+			for(int j = 0; j < exp2.length; j++) {
+				int newexp = exp[i]+exp2[j];
+				int index = check(sumexp, newexp);
+				if(index == -1) {
+					sumexp[sum_p] = newexp;
+					sumcoeffs[sum_p] = coeffs[i] * coeffs2[j];
+					++sum_p;
+				}
+				else {
+					sumcoeffs[index] = sumcoeffs[index] + (coeffs[i] * coeffs2[j]);
+				}
+			}
+		}
+		
+		int i = 0;
+		while(sumexp[i] != 0) {
+			++i;
+		}
+		
+		return new Polynomial(sumcoeffs,sumexp);
 	}
 	
 }
